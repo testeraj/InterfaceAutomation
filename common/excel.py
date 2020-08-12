015a2c12a2ddb09bf030f421ad3fb162d201ea82
testeraj <x1124292446@163.com>
2020-08-12 14:15:44 +0800

    update

--- common/excel.py
+++ common/excel.py
@@ -1,23 +1,36 @@
 import xlwings as xw
 
-app = xw.App(visible=True, add_book=False)
-# app.display_alerts = False
-# app.screen_updating = False
-wb = app.books.open('test.xlsx')   # 打开已有文件
-sht = wb.sheets['sheet1']
-# sht.range('A1').value = [[1], [2], [3]]
-a = sht.range('A1').expand().value
-print(a)
-info = sht.used_range
-row = info.last_cell.column  # 列数
-nrows = info.last_cell.row   # 行数
-print(nrows, row)
-wb.save()
-wb.close()
-app.quit()
 
-# wb = app.books.add()    # 新建文件
-# wb.save(r'test.xlsx')
-# wb.close()
-# app.quit()
+class OpenExecl:
 
+    def __init__(self, file=None, sheet_name='sheet1', visible=False):
+        """
+        :param file: 文件路径，如果文件不存在，则新建文件
+        :param sheet_name: 工作页名称
+        :param visible: bool, 该值决定是否打开应用程序(execl) 默认False
+        """
+        self.__app = xw.App(visible=visible, add_book=False)
+        self.__app.display_alerts = False
+        self.__app.screen_updating = False
+        if file:
+            self.workbook = self.__app.books.open(file)          # 打开已有文件
+        else:
+            self.workbook = self.__app.books.add()                   # 新建文件
+        self.sheet = self.workbook.sheets[sheet_name]
+        info = self.sheet.used_range
+        self.row = info.last_cell.column  # 列数
+        self.nrows = info.last_cell.row  # 行数
+
+    def readexecl(self, start='A1'):
+        data = self.sheet.range(start).expand().value
+        return data
+
+    def writeexecl(self, data, start='A1'):
+        self.sheet.range(start).value = [[1, 'AJ', 3], [2, 'KB', 10], [3, 'h', 51], [4, 9, 00]]    # 一个二维列表对应一行
+        self.sheet.range(start).value = [[1], [2], [3], [4]]    # 对列
+        self.sheet.range(start).value = [1, 2, 3, 4]     # 对行
+
+    def close(self):
+        self.workbook.save()
+        self.workbook.close()
+        self.__app.quit()
