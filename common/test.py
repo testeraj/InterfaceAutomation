23b8f6769b3ddd993ddd610eec519e4b94032eba
testeraj <x1124292446@163.com>
2020-08-13 22:26:09 +0800

    update

--- common/test.py
+++ common/test.py
@@ -1,10 +1,26 @@
-def data(data, isrow=True):
-    print(len(data))
-    print(([le for le in list(map(lambda x: len(x), [row for row in data])) if len(data[0]) == le]))
-    if len(data) != len([le for le in list(map(lambda x: len(x), [row for row in data])) if le == len(data[0])]):
-        print(1)
+# 子生成器
+def average_gen():
+    total = 0
+    count = 0
+    average = 0
+    while True:
+        new_num = yield average
+        count += 1
+        total += new_num
+        average = total/count
 
+# 委托生成器
+def proxy_gen():
+    while True:
+        yield from average_gen()
 
+# 调用方
+def main():
+    calc_average = proxy_gen()
+    print(next(calc_average))            # 预激下生成器
+    print(calc_average.send(10))  # 打印：10.0
+    print(calc_average.send(20))  # 打印：15.0
+    print(calc_average.send(30))  # 打印：20.0
 
-data([[1, 2, 3], [1, 6]])
-# data([5, 9])
+if __name__ == '__main__':
+    main()
