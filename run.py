bdf202bfce1becc1a3351b6f9c48b7f7bd3d7acc
testeraj <x1124292446@163.com>
2020-09-04 11:12:16 +0800

    update

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
 import os
 import pytest
+from common.sendemails import Email
+from common.readdata import EMAIL_CONFIG
+
 
 if __name__ == '__main__':
-
+    path3 = 'F:/InterfaceAutomation/report.html'
+    if os.path.exists(path3):
+        os.remove(path3)
     pytest.main()
-    os.system('allure generate allure-result -o allure-report --clean')
+    os.system('allure generate allure-result -o allure-report')
+    # Email(EMAIL_CONFIG).send_mail('F:/InterfaceAutomation/report.html')
     # os.system('allure serve ./allure-result')
 
